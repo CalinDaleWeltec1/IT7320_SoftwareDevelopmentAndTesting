0718ed6e48d56edb4147a25b2b95d9abe40a5e60
Calin.Dale <Calin.Dale@B204-03.soit.local>
2018-07-25 14:05:42 +1200

    First Commit

---
+++ Assignment 1/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	private String stuff;
+	private int stuff2;
+	
+	public String getStuff() {
+		return stuff;
+	}
+	public void setStuff(String stuff) {
+		this.stuff = stuff;
+	}
+	public int getStuff2() {
+		return stuff2;
+	}
+	public void setStuff2(int stuff2) {
+		this.stuff2 = stuff2;
+	}
+}
